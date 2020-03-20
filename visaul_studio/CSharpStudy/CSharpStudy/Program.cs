71d438ff89dd5688afc27e142a19a039029e907b
kimtaewoongg <rlaxodnduobv@naver.com>
2020-03-20 21:32:53 +0900

    3days

---
+++ visaul_studio/CSharpStudy/CSharpStudy/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕! C#");
+            Console.Write("Hello, C#");
+        }
+    }
+}
