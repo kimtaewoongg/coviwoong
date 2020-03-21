cd53e2fcfdc8a548d9aa80ee4a95132b6d8ebcdf
kimtaewoongg <rlaxodnduobv@naver.com>
2020-03-21 20:50:31 +0900

    4days

--- visaul_studio/CSharpStudy/CSharpStudy/Program.cs
+++ visaul_studio/CSharpStudy/CSharpStudy/Program.cs
@@ -12,6 +12,83 @@ namespace CSharpStudy
         {
             Console.WriteLine("안녕! C#");
             Console.Write("Hello, C#");
+
+            //1 2 3 4 5 6 ... 10
+
+            //var num = 1;
+
+            //Generic List
+            List<string> list = new List<string>();
+            list.Add("text1");
+            list.Add("text2");
+            list.Add("text3");
+            list.Add("text4");
+            list.Add("text5");
+
+            foreach (var text in list)
+            {
+                Console.WriteLine(text);
+            }
+
+            var listVar = new List<string>()
+            {
+                "text1",
+                "text2",
+                "text3",
+                "text4",
+                "text5"
+            };
+
+            foreach (var text in listVar)
+            {
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            //번호    이름   나이  연락처
+            //01     김태웅  28    010-1234-1234
+            //02     김해웅  27    010-9999-8888
+
+
+            var user1 = new User()
+            {
+                No = 1,
+                Name = "김태웅",
+                Age = 28,
+                Phone = "010-1234-1234"
+            };
+
+            //user1.No = 1;
+            //user1.Name = "김태웅";
+
+            var user2 = new User()
+            {
+                No = 2,
+                Name = "김해웅",
+                Age = 27,
+                Phone = "010-8888-9999"
+            };
+
+            //user2.No = 2;
+            //user2.Name = "김해웅";
+
+            var listInfo = new List<User>()
+            {
+                user1, user2
+            };
+
+            //var listInfo = new List<User>();
+            //listInfo.Add(user1);
+            //listInfo.Add(user2);
+
+            foreach (var user in listInfo)
+            {
+                Console.WriteLine("번호 : " + user.No + " / " + "이름 : " + user.Name + "/ 나이 : "+ user.Age + " / 연락처 : " + user.Phone );
+            }
+
+
         }
     }
 }
---
+++ visaul_studio/CSharpStudy/CSharpStudy/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    public class User
+    {
+        //번호, 이름, 나이, 연락처
+        //C# 3.0 이상의 기능
+        public int No { get; set; }
+
+        public string Name{ get; set; }
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; }
+
+
+        //private int _no;
+
+        //private string _name;
+
+        ////Java 형식 get set
+        //public void setNo(int no)
+        //{
+        //    _no = no;
+        //}
+
+        //public int getNo()
+        //{
+        //    return _no;
+        //}
+    }
+}
