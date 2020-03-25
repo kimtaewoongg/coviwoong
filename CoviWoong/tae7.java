8691869aae95edd8850beab9d672a8f303471ee4
kimtaewoongg <rlaxodnduobv@naver.com>
2020-03-25 22:45:49 +0900

    import and package

---
+++ CoviWoong/tae7.java
@@ -0,0 +1,17 @@
+import java.util.Date;	//ctrl + shift + o 단축기로 필요한 import
+
+// jar파일은 zip같은 역할이며, class파일 묶어놓은 것!S
+//import java.util.Date;
+
+public class tae7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date today = new Date();
+		System.out.println(today);
+		
+		// if no import
+		// java.util.Date today = new java.util.Date();
+	}
+
+}
---
+++ CoviWoong/tae8.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.random;
+import static java.lang.System.out;
+public class tae8 {
+
+	public static void main(String[] args) {
+		
+		// System.out.println(Math.random());
+		out.println(random());
+		
+	}
+
+}
